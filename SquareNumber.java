422fe52e8471e76d7d010044f8f11ab39a440e35
ashuuydv <147934359+Ashuuydv@users.noreply.github.com>
2024-03-03 16:28:49 +0530

    1 2 3 4 1 2 3 4 1 2 3 4 1 2 3 4

---
+++ SquareNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class SquareNumber{
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.print("enter the length of square : ");
+        int m = read.nextInt();
+        for(int i = 0; i < m; i++){
+            System.out.println();
+            for(int j = 1; j <= m; j++){
+                System.out.print(j+" ");
+            }
+        }
+    }
+}
